84644e123a1df7278505acd61c55858d09782253
kitoken99 <kitoken99@gmail.com>
2023-07-15 02:45:42 +0000

    showの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,13 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to :books
-  end
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
     
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
   
   
   
