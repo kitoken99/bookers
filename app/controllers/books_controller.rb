377d04577d73ac1f2d35795b0821c1ce688be897
kitoken99 <kitoken99@gmail.com>
2023-07-15 03:16:13 +0000

    editの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,19 @@ class BooksController < ApplicationController
     book.save
     flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
-    
   end
   def show
     @book = Book.find(params[:id])
   end
-  
-  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
   
   
   private
